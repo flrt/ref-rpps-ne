0918ebba74b5ac2b4a595761d7a08079ec24b08b
frederic.laurent@gmail.com <H0bb3s+367>
2021-05-19 11:33:04 +0200

    changement suffixes

--- run.sh
+++ run.sh
@@ -4,12 +4,14 @@ export PYTHONPATH=/opt
 
 echo "Run Look out RPPS"
 python lookout.py --conf conf/config-rpps.json
-#\
+# uncomment for ftp upload
+# \
 #                     --dataftp conf/ftp-data-rpps.json \
 #                     --feedftp conf/ftp-feed-rpps.json
 
 echo "Run Look out MSSante"
 python lookout.py --conf conf/config-mssante.json
+# uncomment for ftp upload
 #\
 #                     --dataftp conf/ftp-data-mssante.json \
 #                     --feedftp conf/ftp-feed-mssante.json
